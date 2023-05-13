0f2e86d69622d94db431019e9d50df753a56574e
FutoonAlShaiddi <114404513+FutoonAlShaiddi@users.noreply.github.com>
2023-05-13 21:52:11 +0300

    Update upcomming_visit_screen.dart

--- lib/Patient-PhysicianScreens/upcomming_visit_screen.dart
+++ lib/Patient-PhysicianScreens/upcomming_visit_screen.dart
@@ -119,8 +119,10 @@ class _UpCommingVisitScreenState extends State<UpCommingVisitScreen> {
         nationality = '${row[5]}';
         maritalStatus = '${row[6]}';
         // age = '${row[7]}';
-        age = DateTime.now().year - int.parse(DOB.substring(0, 4));
-        if (int.parse(DOB.substring(5, 7)) >= DateTime.now().month) {
+       age = DateTime.now().year - int.parse(DOB.substring(0, 4));
+        if (int.parse(DOB.substring(5, 7)) > DateTime.now().month) {
+          age = age - 1;
+        } else if (int.parse(DOB.substring(5, 7)) == DateTime.now().month) {
           if (int.parse(DOB.substring(8, 10)) > DateTime.now().day) {
             age = age - 1;
           }
